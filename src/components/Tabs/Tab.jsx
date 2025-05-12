3de6b3b6a6d87ffd7b6c649a614494ea25de8e86
jacknycz <jack.nycz@gmail.com>
2025-05-12 09:55:50 -0700

    Pointer update

--- src/components/Tabs/Tab.jsx
+++ src/components/Tabs/Tab.jsx
@@ -10,7 +10,7 @@ export default function Tab({ children, icon, isActive, onSelect, id, panelId })
             aria-controls={panelId}
             tabIndex={isActive ? 0 : -1}
             onClick={onSelect}
-            className={`flex items-center gap-2 px-4 py-2 text-sm font-medium border-b-2 transition-colors duration-200
+            className={`flex cursor-pointer items-center gap-2 px-4 py-2 text-sm font-medium border-b-2 transition-colors duration-200
           ${isActive
                     ? 'border-p-50 text-p-50 dark:text-white'
                     : 'border-transparent text-gray-600 dark:text-p-90 dark:hover:text-white hover:border-p-60'}
