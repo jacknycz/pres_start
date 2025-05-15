6b54c32d4a6547c8ccb19487396ee3a78e9f2c03
jacknycz <jack.nycz@gmail.com>
2025-05-15 09:32:38 -0700

    opacity

--- src/components/ThemeColorPicker.jsx
+++ src/components/ThemeColorPicker.jsx
@@ -73,7 +73,7 @@ export default function ThemeColorPicker({ isModal = false, onClose }) {
   if (isModal) {
     return (
       <AnimatePresence>
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
+        <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50 p-4">
           <motion.div 
             initial={{ opacity: 0, scale: 0.95, y: 20 }}
             animate={{ opacity: 1, scale: 1, y: 0 }}
